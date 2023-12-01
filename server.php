04fdb54d61f8281c333c791e36a6c28ad6b2a4a7
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-12-01 16:43:04 +0100

    added correctly task

--- server.php
+++ server.php
@@ -2,10 +2,24 @@
 $filecontent = file_get_contents("todo-list.json");
 $list = json_decode($filecontent, true);
 
-if (isset($_POST['task'])) {
+if (!empty($_POST['task'])) {
     $task = $_POST['task'];
-
-    array_unshift($list, $task);
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $image = $_POST['image'];
+    $priority = $_POST['priority'];
+    $text = $_POST['text'];
+    $doneTask = $_POST['doneTask'];
+    $newTask = [
+        'task' => $task,
+        'id' => intval($id),
+        'name' => $name,
+        'image' => $image,
+        'priority' => $priority,
+        'text' => $text,
+        'doneTask' => $doneTask
+    ];
+    array_unshift($list, $newTask);
     file_put_contents("todo-list.json", json_encode($list));
 }
 header('Content-Type: application/json');
